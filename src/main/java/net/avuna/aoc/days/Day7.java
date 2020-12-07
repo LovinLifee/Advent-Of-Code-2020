b07fd220c54a086a02b61e9a72e4dbcf731ac262
Hayden <hayden@avuna.net>
2020-12-07 05:43:16 -0500

    day 7 part 2 refactored - suck it shrimpy

--- src/main/java/net/avuna/aoc/days/Day7.java
+++ src/main/java/net/avuna/aoc/days/Day7.java
@@ -6,13 +6,8 @@ import net.avuna.aoc.Challenge;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Day7 extends Challenge<Integer> {
-
-    private static final Pattern bagPattern = Pattern.compile("(.+) bags contain (.+)\\.");
-    private static final Pattern partPattern = Pattern.compile("(\\d+?) (.+?) bags?");
 
     private final Map<String, Bag> bags = parseBags();
 
@@ -29,7 +24,6 @@ public class Day7 extends Challenge<Integer> {
 
     @Override
     public Integer doPartTwo() {
-        int count = 0;
         Bag b = bags.get("shiny gold");
         return countChildrenBags(b) - 1;
     }
@@ -37,19 +31,16 @@ public class Day7 extends Challenge<Integer> {
     private Map<String, Bag> parseBags() {
         final Map<String, Bag> bags = new HashMap<>();
         readLines().forEach(line -> {
-            Matcher bagMatcher = bagPattern.matcher(line);
-            bagMatcher.matches();
-            Bag bag = bags.computeIfAbsent(bagMatcher.group(1), Bag::new);
-            String data = bagMatcher.group(2);
-            if (data.equals("no other bags"))
-                return;
-            String[] split = data.split(", ");
-            for (String part : split) {
-                Matcher partMatcher = partPattern.matcher(part);
-                partMatcher.matches();
-                int num = Integer.parseInt(partMatcher.group(1));
-                Bag child = bags.computeIfAbsent(partMatcher.group(2), Bag::new);
-                bag.children.put(child.getColor(), num);
+            String[] parts = line.split(" bags contain ");
+            Bag bag = bags.computeIfAbsent(parts[0], Bag::new);
+            String[] childParts = parts[1].split(", ");
+            for(String s : childParts) {
+                if(s.startsWith("no"))
+                    continue;
+                int amount = Integer.parseInt(s.substring(0, 1));
+                String childColor = s.substring(2, s.length() - (amount > 1 ? 5 : 4)).trim();
+                Bag child = bags.computeIfAbsent(childColor, Bag::new);
+                bag.children.put(child.getColor(), amount);
             }
         });
         return bags;
